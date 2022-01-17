853a814e33cf7fdb7df4bbd8d591bd11155da14f
aroary <meanchemshkedi@gmail.com>
2022-01-17 14:55:39 -0500

    fix semicolons

--- index.py
+++ index.py
@@ -1,7 +1,7 @@
 def main():
     print("Legit Programming Todo-App!\n")
-    print("(A)dd Todo, (V)iew Todos, (M)ark Todo, (U)pdate Todo");
-    print("Enter a command: ");
+    print("(A)dd Todo, (V)iew Todos, (M)ark Todo, (U)pdate Todo")
+    print("Enter a command: ")
 
 if __name__ == "__main__":
     main()
