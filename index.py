c4a1ba2bcde715af810523fe403fa0a5fbae93f8
Atticus C <atticus@softwarefuze.com>
2022-01-17 09:17:10 -0800

    Print information

--- index.py
+++ index.py
@@ -1,1 +1,7 @@
-# index file
+def main():
+    print("Legit Programming Todo-App!\n")
+    print("(A)dd Todo, (V)iew Todos, (M)ark Todo, (U)pdate Todo");
+    print("Enter a command: ");
+
+if __name__ == "__main__":
+    main()
