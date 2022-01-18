28c1b680ae3cbed2d5988485eac41e4fe908c80a
aroary <meanchemshkedi@gmail.com>
2022-01-18 11:10:14 -0500

    show [0] alias on help

--- index.py
+++ index.py
@@ -17,7 +17,7 @@ def main():
         # Check for help and exit.
         if command == "help" or command == "h":
             for i in commands:
-                print(f"{i['name']}: {i['description']}")
+                print(f"({i['alias'][0]}) {i['name']}: {i['description']}")
         elif command == "exit" or command == "e":
             print("Exiting...")
             break
