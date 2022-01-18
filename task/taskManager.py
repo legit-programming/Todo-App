abcf4c11fd911deabfbb8caea6db1a71673d5d4b
aroary <meanchemshkedi@gmail.com>
2022-01-18 09:43:30 -0500

    sort by

--- task/taskManager.py
+++ task/taskManager.py
@@ -5,9 +5,30 @@ from uncompleteTask import uncompleteTask
 from listTasks import listTasks
 
 commands = [
-    {"name": "add", "description": "Add a task", "alias": ["a"], "function": lambda: addTask(input("Task: "), input("Description: "), int(input("Priority: ")))},
-    {"name": "remove", "description": "Remove a task", "alias": ["r"], "function": lambda: removeTask(int(input("ID: ")))},
-    {"name": "complete", "description": "Complete a task", "alias": ["c", "resolve"], "function": lambda: completeTask(int(input("ID: ")))},
-    {"name": "uncomplete", "description": "Uncomplete a task", "alias": ["u", "incomplete"], "function": lambda: uncompleteTask(int(input("ID: ")))},
-    {"name": "list", "description": "List the active tasks", "alias": ["l"], "function": lambda: listTasks()}
+    {
+        "name": "add",
+        "description": "Add a task",
+        "alias": ["a"],
+        "function": lambda: addTask(input("Task: "), input("Description: "), int(input("Priority: ")))
+    },
+    {
+        "name": "remove", "description": "Remove a task", "alias": ["r"], "function": lambda: removeTask(int(input("ID: ")))},
+    {
+        "name": "complete",
+        "description": "Complete a task",
+        "alias": ["c", "resolve"],
+        "function": lambda: completeTask(int(input("ID: ")))
+    },
+    {
+        "name": "uncomplete",
+        "description": "Uncomplete a task",
+        "alias": ["u", "incomplete"],
+        "function": lambda: uncompleteTask(int(input("ID: ")))
+    },
+    {
+        "name": "list",
+        "description": "List the active tasks",
+        "alias": ["l"],
+        "function": lambda: listTasks(input("(priority | date | id) Sort by: "), input("(up | down) Direction: "))
+    }
 ]
