c2dafa06260851bb2f34f3cc4b948743f529495a
aroary <meanchemshkedi@gmail.com>
2022-01-18 11:15:48 -0500

    remove int ize restioctions

--- task/removeTask.py
+++ task/removeTask.py
@@ -3,10 +3,6 @@ from todoData import getTodoData, setTodoData
 def removeTask(id):
     if "-" not in id and "." not in id and id.isdigit():
         id = int(id)
-        
-        if id < 0 or id > 4:
-            print("ID must be between 0 and 4.")
-            return
     else:
         print("Invalid id.")
         return
