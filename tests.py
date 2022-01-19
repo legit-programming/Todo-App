4b56b3dd9b3ac08db2438b6fd2cf3461d12ee5b2
aroary <meanchemshkedi@gmail.com>
2022-01-19 00:07:34 -0500

    test set up for new dataformat

--- tests.py
+++ tests.py
@@ -1,18 +1,20 @@
 print("Initializing checks")
 
+from types import SimpleNamespace
 import json
 import os
 import sys
-from types import SimpleNamespace
 sys.path.append('./task')
-from taskManager import commands
+sys.path.append('./workspace')
+import taskManager
+import workspaceManager
 
 # Check for duplicate commands.
 print("Checking for duplicate commands\n")
-for i in commands:
+for i in workspaceManager.commands + taskManager.commands:
     print(i)
 
-    for j, o in enumerate(commands):
+    for j, o in enumerate(workspaceManager.commands + taskManager.commands):
         print("comparing: " + i["name"] + " and " + o["name"])
         
         if o["name"] != i["name"]:
@@ -26,18 +28,7 @@ with open(os.path.dirname(os.path.realpath(__file__)) + "/todo.json") as file:
     todoData = json.load(file, object_hook=lambda d: SimpleNamespace(**d))
     print(todoData)
 
-    print("Checking id\n")
-    if todoData.id != 0:
-        raise Exception(f"ID is not 0 (todo.json): {todoData.id}")
+    if len(todoData):
+        raise Exception("Array in todo.json must be empty")
 
-    print("Checking tasks\n")
-    if len(todoData.tasks):
-        raise Exception(f"Tasks is not empty (todo.json): {todoData.tasks}")
-
-    print("Checking completed\n")
-    if len(todoData.completed):
-        raise Exception(f"Completed is not empty (todo.json): {todoData.completedTasks}")
-
-    print("todo.json is clean\n")
-
-    print("Checks completed")
+print("Checks completed")
