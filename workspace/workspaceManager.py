c661272b14b7d373fedf32aa0c8d211ad0197859
aroary <meanchemshkedi@gmail.com>
2022-01-19 00:08:03 -0500

    workplace functions

---
+++ workspace/workspaceManager.py
@@ -0,0 +1,35 @@
+from openWorkspace import openWorkspace
+from closeWorkspace import closeWorkspace
+from addWorkspace import addWorkspace
+import os
+
+commands = [
+    {
+        "name": "open",
+        "description": "Open a workspace",
+        "alias": ["o"],
+        "help": "",
+        "function": lambda: openWorkspace(input("Workspace name: "))
+    },
+    {
+        "name": "close",
+        "description": "Close a workspace",
+        "alias": [],
+        "help": "",
+        "function": lambda: closeWorkspace()
+    },
+    {
+        "name": "create",
+        "description": "Add a new workspace",
+        "alias": [],
+        "help": "(string) Workspace name: [the name of the workspace]\n(yes, no) Open: [(y) yes | (n) no]",
+        "function": lambda: addWorkspace(input("Workspace name: "), input("Open: "))
+    },
+    {
+        "name": "current",
+        "description": "Show the current workspace",
+        "alias": ["opend"],
+        "help": "",
+        "function": lambda: print(os.environ["workspace"] or "No workspace opend")
+    }
+]
