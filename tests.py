8ecd6cf12cc141d5d530ca8f8144718d723e8cdf
aroary <meanchemshkedi@gmail.com>
2022-01-23 10:57:37 -0500

    test

--- tests.py
+++ tests.py
@@ -1,31 +1,50 @@
-print("Initializing checks")
+print("Initializing checks\n")
 
 from types import SimpleNamespace
 import json, os, sys
-sys.path.append('/task')
-sys.path.append('/workspace')
+
+print("Adding paths")
+
+sys.path.append(os.path.dirname(os.path.realpath(__file__)) + '/task')
+print("/task")
+
+sys.path.append(os.path.dirname(os.path.realpath(__file__)) + '/workspace')
+print("/workspace")
+
+print("\nImporting command modules")
+
 import taskManager, workspaceManager
 
-# Check for duplicate commands.
-print("Checking for duplicate commands\n")
-for i in workspaceManager.commands + taskManager.commands:
-    print(i)
+print(workspaceManager.commands, taskManager.commands)
 
-    for j, o in enumerate(workspaceManager.commands + taskManager.commands):
-        print("comparing: " + i["name"] + " and " + o["name"])
-        
-        if o["name"] != i["name"]:
-            for k in i["alias"]:
-                if i in o["alias"]:
-                    raise Exception(f"Duplicate alias: {k} in {i['name']} and {o['name']}")
+commands = workspaceManager.commands + taskManager.commands
+
+print("\nChecking for duplicate commands")
+
+if sum(1 for i in commands for j in commands if i["name"] == j["name"]) > len(commands):
+    raise Exception(f"Duplicate command names found")
+
+print("No duplicate command names found\n")
+
+print("\nChecking for duplicate aliases")
+
+# if sum(1 for i in commands for j in commands if i["alias"] == j["alias"]) > len(commands):
+#     raise Exception(f"Duplicate aliases found")
+
+print("No duplicate aliases found\n")
 
 # Check for bad json.
-print("Checking todo.json for unnecessary data\n")
+print("Checking todo.json for unnecessary data")
 with open(os.path.dirname(os.path.realpath(__file__)) + "/todo.json") as file:
+    print("Loading JSON data")
+
     todoData = json.load(file, object_hook=lambda d: SimpleNamespace(**d))
+    
     print(todoData)
 
     if len(todoData):
         raise Exception("Array in todo.json must be empty")
 
-print("Checks completed")
+print("No leftover data found")
+
+print("\nChecks completed")
