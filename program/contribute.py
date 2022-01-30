ec3cf7a6f2ac9cefcd452198dd9363406b3c6015
aroary <71286008+aroary@users.noreply.github.com>
2022-01-29 19:22:47 -0500

    fix fn

--- program/contribute.py
+++ program/contribute.py
@@ -1,2 +1,2 @@
-def contrivute():
-    print("\nLegit Programming Todo-App\nOrganizarion: https://github.com/legit-programming/\nRepository: https://github.com/legit-programming/todo-app\n\n")
+def contribute():
+    print("Legit Programming Todo-App\nOrganizarion: https://github.com/legit-programming/\nRepository: https://github.com/legit-programming/todo-app")
